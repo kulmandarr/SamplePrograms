d6b65422a73cf64a0f49f2bcdfc38ca52d83a0f1
Mandar Kulkarni <makulkarni@microsoft.com>
2024-02-20 16:18:57 +0530

    fixed the armstrong number function

--- LogicPrograms/Programs/NumberPrograms/armstrongNumber.js
+++ LogicPrograms/Programs/NumberPrograms/armstrongNumber.js
@@ -5,15 +5,7 @@ const armstrongNumber = (num1) => {
   for(let i = 0; i< digit.length; i++){
     sum = sum + Math.pow(digit[i], order)
   }
-  let digitSum = String(sum).split('',16)
-  let newSum = 0
-  for(let i = 0; i< digitSum.length; i++){
-    newSum = newSum + digitSum[i]
-  }
-  if(newSum == num1)
-    return "number is armstrong"
-  else
-    return "number is not armstrong"
+  return sum == num1 ? "number is armstrong" : "number is not armstrong"
 }
-const result = armstrongNumber(4679307774)
+const result = armstrongNumber(1634)
 console.log(result)
